8398f8e3038f1c1f0ac9dc4321d01b1f963fa4f2
buenorc <52174075+buenorc@users.noreply.github.com>
2023-06-21 00:16:23 -0300

    06212003

--- layout/scripts/script.js
+++ layout/scripts/script.js
@@ -51,3 +51,48 @@ var form = document.getElementById("my-form");
    });
  }
  form.addEventListener("submit", handleSubmit)
+
+ /* Slideshow */
+
+ var slideIndex = 1;
+ showSlides(slideIndex);
+
+ function plusSlides(n) {
+   showSlides(slideIndex += n);
+ }
+
+ function currentSlide(n) {
+   showSlides(slideIndex = n);
+ }
+
+ function showSlides(n) {
+   var i;
+   var slides = document.getElementsByClassName("mySlides");
+   var dots = document.getElementsByClassName("dot");
+   if (n > slides.length) {slideIndex = 1}
+   if (n < 1) {slideIndex = slides.length}
+   for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";
+   }
+   for (i = 0; i < dots.length; i++) {
+       dots[i].className = dots[i].className.replace(" active", "");
+   }
+   slides[slideIndex-1].style.display = "block";
+   dots[slideIndex-1].className += " active";
+ }
+ document.onkeydown = checkKey;
+
+ function checkKey(e) {
+
+     e = e || window.event;
+
+     if (e.keyCode == '37') {
+        // left arrow
+        plusSlides(-1);
+     }
+     else if (e.keyCode == '39') {
+        // right arrow
+        plusSlides(1);
+     }
+
+ }
